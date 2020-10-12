874e393af74db6c4864e4fd7534a59d5c5ce73c8
adithyavenkatesh <adithyav211@gmail.com>
2020-10-12 10:00:00 -0400

    Generated outline of each state on the leaflet map

---
+++ Adithya/static/js/logic.js
@@ -0,0 +1,117 @@
+// Creating our initial map object
+var mapboxAccessToken = "pk.eyJ1IjoiYWRpdGh5YXYiLCJhIjoiY2tmb2pqcmJsMXd6czJzbWpsYnhsZTVvbiJ9.XsNkSPXCk0EJ6lnjsNdU8Q";
+
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var map = L.map('map').setView([39.82, -98.58], 5);
+
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+    id: 'mapbox/light-v9',
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+// Use this link to get the geojson data.
+var link = "static/data/statesData.geojson";
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(map);
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Grab data with d3
+// d3.json(statesData, function(data) {
+
+//   // Create a new choropleth layer
+//   geojson = L.choropleth(data, {
+
+//     // Define what  property in the features to use
+//     valueProperty: "MHI2016",
+
+//     // Set color scale
+//     scale: ["#ffffb2", "#b10026"],
+
+//     // Number of breaks in step range
+//     steps: 10,
+
+//     // q for quartile, e for equidistant, k for k-means
+//     mode: "q",
+//     style: {
+//       // Border color
+//       color: "#fff",
+//       weight: 1,
+//       fillOpacity: 0.8
+//     },
+
+//     // Binding a pop-up to each layer
+//     onEachFeature: function(feature, layer) {
+//       layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
+//         "$" + feature.properties.MHI2016);
+//     }
+// }).addTo(map);
+
+//     var legend = L.control({ position: "bottomright" });
+//   legend.onAdd = function() {
+//     var div = L.DomUtil.create("div", "info legend");
+//     var limits = geojson.options.limits;
+//     var colors = geojson.options.colors;
+//     var labels = [];
+
+//     // Add min & max
+//     var legendInfo = "<h1>Median Income</h1>" +
+//       "<div class=\"labels\">" +
+//         "<div class=\"min\">" + limits[0] + "</div>" +
+//         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+//       "</div>";
+
+//     div.innerHTML = legendInfo;
+
+//     limits.forEach(function(limit, index) {
+//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+//     });
+
+//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+//     return div;
+//   };
+
+//   // Adding legend to the map
+//   legend.addTo(map);
+
+// });
