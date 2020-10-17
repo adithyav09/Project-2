7db1d8daeb9b48b7d4781f9137ac7867fa45b975
Aaron Eaker <aaroneaker@hotmail.com>
2020-10-16 21:26:04 -0400

    more flask sqlalchemy work

--- AaronApp.py
+++ AaronApp.py
@@ -3,36 +3,17 @@ import http.client
 from flask import Flask, render_template,redirect,request
 import sqlite3
 import json
-from sqlalchemy import create_engine
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine, extract
 from sqlalchemy.orm import Session
-from statefips import States_fipsId
-import choropleth
-import Scrape
-import requests
-import http.client
-import pymongo
+from init_db import HistGasPrices
 
-# def get_db_connection():
-#     conn = sqlite3.connect('gasdatabase.db')
-#     conn.row_factory = sqlite3.Row
-#     return conn
 
-###Testing database  SQL queries in terminal
-# conn = get_db_connection()
-# curs = conn.cursor()
-# currentprices = curs.execute("""SELECT * FROM hist_gas_prices WHERE Date LIKE 2020""")
-# for row in currentprices:
-#     print(*row)
-# conn.close()
 
-##Query some region stuff?
-# def getregion():
-#     conn = get_db_connection()
-#     chosenregion = conn.execute('SELECT (Date,' + regioninput + '') FROM hist_gas_prices WHERE ')
+
 
 # create instance of Flask app
 app = Flask(__name__)
+
 
 # create route that renders index.html template
 @app.route("/", methods=["GET","POST"])
@@ -55,10 +36,9 @@ def index():
         res = conn.getresponse()
         data = res.read()
         Gdata = json.loads(data)
-        print(Gdata)
         Gdata = Gdata['result']
-
         return render_template("index.html", Gasoline_data=Gdata) 
+    
     else:
         zipcode = request.form['zipcode']
         zipapi = "ZVp42kUmPdgG4Na8hYyYEPlhx6cwKcmVZcrDu69nRtPLxcRO92qZFTrkipqJD8dy"
@@ -77,71 +57,69 @@ def index():
         res = conn.getresponse()
         data = res.read()
         Gdata = json.loads(data)
-        print(Gdata)
         Gdata = Gdata['result']
         
         return render_template("index.html", Gasoline_data=Gdata)
 
-<<<<<<< HEAD
-# @app.route("/charts.html")
-# def choropleth():
-#     return render_template('charts.html')
-# Initialize PyMongo to work with MongoDBs
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
 
-# Define database and collection
-db = client.Gas_Data
-collection = db.State_Data
-@app.route("/News.html")
-=======
 @app.route("/Charts")
-def choropleth():
-<<<<<<< HEAD
-    
-=======
-<<<<<<< HEAD
-    
-    return render_template('charts.html',)
-=======
->>>>>>> 0e24a511e10f1365cd24f8258bc15efe2c3c2a52
+def addnew():
     return render_template('charts.html')
->>>>>>> 94146e0f3597178f2593b968e4030f8e2007d1ed
 
 @app.route("/News")
->>>>>>> 5582ea3f22d3b5b2f9b8a6f6232fed6db50b845f
 def addNews():
-    Data = Scrape.scrape_gas() 
-    # collection.insert_one(Data)
-    collection.update({},Data,upsert=True)
-    # Scrape_Data = Data.find_one()
+    return render_template('News.html')
 
-    return render_template('News.html',Gas_info=Data)
 
-@app.route("/Years")
+@app.route("/Years", methods=["GET","POST"])
 def Years():
-    Base = automap_base()
     engine = create_engine('sqlite:///gasdatabase.db')
-    Base.prepare(engine, reflect = True)
-    HistGasPrices = Base.classes.hist_gas_prices
     session = Session(engine)
     histgasprices = session.query(HistGasPrices)
-    
-    return render_template('Years.html', histgasprices=histgasprices)
 
-# @app.route("/Years")
-# def Years():
-#     conn = get_db_connection()
-#     currentprices = conn.execute('SELECT * FROM hist_gas_prices WHERE Date LIKE 2020*').fetchall()
-#     histprices = conn.execute('SELECT * FROM hist_gas_prices WHERE Date LIKE ' + yearinput + '*').fetchall()
-#     conn.close()
-#     yearinput = request.form['text']
-#     return render_template('Years.html')
+
+    if request.method == "GET":
+        currentyearprices = "2020"
+        Query = histgasprices.filter(HistGasPrices.Year == currentyearprices)   
+        for r in Query:
+            Years = [r.Year for r in Query]
+            Dates = [r.Date for r in Query]
+            NEPrices = [r.New_England_Prices for r in Query]
+            CAPrices = [r.Central_Atlantic_Prices for r in Query]
+            LAPrices = [r.Lower_Atlantic_Prices for r in Query]
+            MWPrices = [r.Midwest_Prices for r in Query]
+            GFPrices = [r.Gulf_Coast_Prices for r in Query]
+            RMPrices = [r.Rocky_Mountain_Prices for r in Query]
+            WCPrices = [r.West_Coast_Prices for r in Query]
+            NoCAPrices = [r.West_Coast_No_Cali_Prices for r in Query]
+            
+        HistoricalData = {"Dates":Dates, "NEPrices":NEPrices, "CAPrices":CAPrices, "LAPrices":LAPrices,"MWPrices":MWPrices, "GFPrices":GFPrices, "RMPrices":RMPrices, "WCPrices":WCPrices, "NoCAPrices":NoCAPrices }
+        return render_template('Years.html', HistoricalData=HistoricalData)
+    
+    else: 
+        useryear = request.form['useryear']
+        Query = histgasprices.filter(HistGasPrices.Year == useryear)
+        for r in Query:
+            Years = [r.Year for r in Query]
+            Dates = [r.Date for r in Query]
+            NEPrices = [r.New_England_Prices for r in Query]
+            CAPrices = [r.Central_Atlantic_Prices for r in Query]
+            LAPrices = [r.Lower_Atlantic_Prices for r in Query]
+            MWPrices = [r.Midwest_Prices for r in Query]
+            GFPrices = [r.Gulf_Coast_Prices for r in Query]
+            RMPrices = [r.Rocky_Mountain_Prices for r in Query]
+            WCPrices = [r.West_Coast_Prices for r in Query]
+            NoCAPrices = [r.West_Coast_No_Cali_Prices for r in Query]
+            
+        HistoricalData = {"Dates":Dates, "NEPrices":NEPrices, "CAPrices":CAPrices, "LAPrices":LAPrices,"MWPrices":MWPrices, "GFPrices":GFPrices, "RMPrices":RMPrices, "WCPrices":WCPrices, "NoCAPrices":NoCAPrices }
+        
+        return render_template('Years.html', HistoricalData=HistoricalData)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
 
 
 
